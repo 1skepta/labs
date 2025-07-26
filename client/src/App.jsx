3acbe4c541677f91570fa8414ebac3e28cd088a8
1skepta <prospaul999@gmail.com>
2025-07-26 09:01:31 +0000

    working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
+import ScrollToTop from "./utils/ScrollToTop";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Layout />
     </BrowserRouter>
   );
